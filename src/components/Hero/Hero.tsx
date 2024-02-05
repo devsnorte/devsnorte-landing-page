5b276b9dc2ce90e770a4df9198befd04aa1ba016
Suami Rocha <suamirochati@gmail.com>
2024-02-05 10:35:54 -0300

    style: Add margin and bring star icon closer to the devsnorte logo - desktop version

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,14 +2,18 @@ import ImageHero from "../ImageHero"
 import css from './style.module.css'
 
 const Hero = () => {
-    return <div className="px-6">
-        <div className="w-[80%] mx-auto my-96 relative">
-            <hr className={`w-20 absolute right-0 -top-10 ${css.line}`} />
-            <ImageHero />
-            <p className={css.text}>A maior comunidade do norte do Brasil</p>
-            <hr className={`w-40 absolute right-0 ${css.line}`} />
+    return (
+      <div className="px-6">
+        <div className="w-[80%] mx-auto my-96 relative md:w-[35%]">
+          <hr className={`w-20 absolute right-1 -top-10 ${css.line}`} />
+          <ImageHero />
+          <p className={`mt-5 ${css.text}`}>
+            A maior comunidade do norte do Brasil
+          </p>
+          <hr className={`w-40 absolute right-0 ${css.line}`} />
         </div>
-    </div>
+      </div>
+    );
 }
 
 export default Hero
