633e744d6f0dcdab92d5626aab0573646d1b2961
Suami Rocha <suamirochati@gmail.com>
2024-02-15 10:20:15 -0300

    feat: add favicon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,12 @@ interface HeaderProps extends React.HTMLAttributes<HTMLElement> {}
 const Header = ({ className = "", ...rest }: HeaderProps) => {
   return (
     <header className={`flex flex-col items-center justify-center px-5`}>
+      <link
+        rel="icon"
+        href="/icons/favicon.svg"
+        type="image/svg"
+        sizes="32x32"
+      />
       <div className="flex justify-between w-full h-16 max-w-screen-lg pt-10">
         <Link href="/">
           <div className="hover:pointer"></div>
