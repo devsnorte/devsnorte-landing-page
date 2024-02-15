ca1bff4f4a42840a552431282f675a25a66f80c4
Suami Rocha <suamirochati@gmail.com>
2024-02-15 08:48:25 -0300

    feat: add animation to subtitle with gsap

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,48 @@
-import ImageHero from "../ImageHero"
+"use client";
+import gsap from "gsap";
+import ImageHero from "../ImageHero";
 import Detail from "./detail";
-import css from './style.module.css'
+import css from "./style.module.css";
+import { useEffect, useRef } from "react";
 
 const Hero = () => {
-    return (
-      <div className="px-6">
-        <div className="mx-auto my-64 relative max-w-screen-md">
-          <hr className={`w-20 absolute right-1 -top-10 ${css.line}`} />
-          <ImageHero />
-          <p className={`mt-5 text-base ${css.text} lg:text-3xl`}>
-            A maior comunidade do norte do Brasil
-          </p>
-          <hr className={`w-40 absolute right-0 ${css.line}`} />
-        </div>
-        <Detail />
-      </div>
-      
-    );
-}
+  const textHeroSubtitleRef = useRef(null);
 
-export default Hero
+  useEffect(() => {
+    const textHeroSubtitle = textHeroSubtitleRef.current;
+
+    gsap.fromTo(
+      textHeroSubtitle,
+      {
+        opacity: 0,
+        duration: 3,
+        y: 0,
+      },
+      {
+        opacity: 1,
+        duration: 2,
+        y: 20,
+        ease: "power4.out",
+      }
+    );
+  }, []);
+
+  return (
+    <div className="px-6">
+      <div className="mx-auto my-64 relative max-w-screen-md">
+        <hr className={`w-20 absolute right-1 -top-10 ${css.line}`} />
+        <ImageHero />
+        <p
+          className={`mt-5 text-base lg:text-3xl opacity-0`}
+          ref={textHeroSubtitleRef}
+        >
+          A maior comunidade do norte do Brasil
+        </p>
+        <hr className={`w-40 absolute right-0 ${css.line}`} />
+      </div>
+      <Detail />
+    </div>
+  );
+};
+
+export default Hero;
