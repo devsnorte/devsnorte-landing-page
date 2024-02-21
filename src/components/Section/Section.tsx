a914f8b2b91d216cdd11d3b2d5e726b9ae00aa1c
juan almeida <juanalgal2@gmail.com>
2024-02-21 18:16:37 -0300

    feat(svg-to-react-component#30): adding svgr

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { Container, ContainerContent, Content, Image, Info, Title } from "./index";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ReactangleSection from "/public/icons/rectangle-section.svg";
+import LineSection from "/public/icons/line-section.svg";
 import { useLandingPageInfos } from "@/hooks/useLandingPageInfos";
 import { useBreakpoints } from "@/hooks/useBreakpoints";
-
-gsap.registerPlugin(ScrollTrigger);
 
 const Section = {
   Container,
@@ -29,19 +27,13 @@ const SectionPrincipal = () => {
         <Section.Content variant={isEven ? "black" : "brand"}>
           <Section.Title>{section.title}</Section.Title>
           <div className="pt-5">
-            <svg width="157" height="1" viewBox="0 0 157 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <line x1="4.37114e-08" y1="0.5" x2="157" y2="0.500014" stroke={isEven ? "#EEEEEE" : "#111111"} />
-            </svg>
+            <LineSection className={`w-[157px] h-[1px] text-${isEven ? "white" : "black"}`} />
           </div>
           <Section.ContainerContent>
             <Section.Info>{section.info}</Section.Info>
           </Section.ContainerContent>
           {shouldSquareAppear && (
-            <div className="absolute bottom-10 -right-5 z-10">
-              <svg width="118" height="158" viewBox="0 0 118 158" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="118" height="158" fill={isEven ? "#41B883" : "#111111"} />
-              </svg>
-            </div>
+            <ReactangleSection className={` absolute bottom-10 -right-5 z-10 w-[118px] h-[158px] text-${isEven ? "brand" : "black"}`} />
           )}
         </Section.Content>
       </Section.Container>
