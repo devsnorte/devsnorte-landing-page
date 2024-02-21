706667b33510d28d2c73e1ff0ffa6de02d0208aa
juan almeida <juanalgal2@gmail.com>
2024-02-20 22:37:09 -0300

    bug(responsiveness-error#13): next window on server side error

--- src/components/Section/Content.tsx
+++ src/components/Section/Content.tsx
@@ -10,7 +10,7 @@ export const Content = ({ variant = "brand", children }: SectionContentProps) =>
 
   return (
     <div
-      className={`p-5 h-[500px] md:h-[835px] md:h-[430px] lg:h-[610px] xl:h-[835px] w-full flex flex-col items-start lg:px-48 justify-center relative ${variantsClasses[variant]}`}>
+      className={`p-5 h-[500px]  md:h-[430px] lg:h-[610px] xl:h-[835px] w-full flex flex-col items-start lg:px-48 justify-center relative ${variantsClasses[variant]}`}>
       <>{children}</>
     </div>
   );
--- src/hooks/useBreakpoints.tsx
+++ src/hooks/useBreakpoints.tsx
@@ -2,22 +2,31 @@ import { useEffect, useState } from "react";
 
 export const useBreakpoints = () => {
   const [windowSize, setWindowSize] = useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
+    height: 0,
+    width: 0,
   });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setWindowSize({
+        height: window?.innerHeight || 0,
+        width: window?.innerWidth || 0,
+      });
+    }
+  }, []);
 
   const handleResize = () => {
     setWindowSize({
-      height: window.innerHeight,
-      width: window.innerWidth,
+      height: window?.innerHeight || 0,
+      width: window?.innerWidth || 0,
     });
   };
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
+    window?.addEventListener("resize", handleResize);
     handleResize();
 
-    return () => window.removeEventListener("resize", handleResize);
+    return () => window?.removeEventListener("resize", handleResize);
   }, [windowSize.width]);
 
   return {
