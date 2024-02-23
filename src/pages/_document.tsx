975a1da7237b9a31a85a4840cf23daeec0a0fb5d
Cleison Carlos <cleisonspider@yahoo.com.br>
2024-02-23 14:36:00 -0300

    feat: improve SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,48 @@
 import type { Metadata } from "next";
 import { Html, Main, Head, NextScript } from "next/document";
 
-export const metadata: Metadata = {
-  title: "Devs Norte",
+const seo = {
+  title: 'Devs Norte',
   description: "A maior comunidade de Desenvolvedores do Norte do Brasil",
-};
+  keywords: ['palavra1', 'palavra2', 'palavra3'],
+  url: "www.devsnorte.com.br",
+  urlImage: "https://devsnorte.netlify.app/_ipx/w_828,q_75/%2Fimages%2Fnetworking.png?url=%2Fimages%2Fnetworking.png"
+}
+
+export const metadata: Metadata = {
+  title: seo.title,
+  description: seo.description,
+  keywords: seo.keywords,
+  openGraph: {
+    title: seo.title,
+    url: seo.url,
+    description: seo.description,
+    type: "website",
+    images: [
+      {
+        url: seo.urlImage,
+        type: "image/jpg",
+        width: 1200,
+        height: 630,
+        alt: seo.title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: seo.title,
+    description: seo.description,
+    images: [
+      {
+        url: seo.urlImage,
+        type: "image/jpg",
+        width: 1200,
+        height: 630,
+        alt: "EMM",
+      },
+    ],
+  },
+}
 
 export default function Document() {
   return (
