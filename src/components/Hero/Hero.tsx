2288d7c6b0937ce9ca7119ab471a1cdab1222e30
Kenny4297 <geckob4i@gmail.com>
2024-02-28 17:44:31 -0600

    Add dark mode

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,41 +7,41 @@ import { useEffect, useRef } from "react";
 import { useTranslation } from "next-i18next";
 
 const Hero = () => {
-  const { t } = useTranslation();
-  const textHeroSubtitleRef = useRef(null);
+    const { t } = useTranslation();
+    const textHeroSubtitleRef = useRef(null);
 
-  useEffect(() => {
-    const textHeroSubtitle = textHeroSubtitleRef.current;
+    useEffect(() => {
+        const textHeroSubtitle = textHeroSubtitleRef.current;
 
-    gsap.fromTo(
-      textHeroSubtitle,
-      {
-        opacity: 0,
-        duration: 3,
-        y: 0,
-      },
-      {
-        opacity: 1,
-        duration: 2,
-        y: 20,
-        ease: "power4.out",
-      }
+        gsap.fromTo(
+            textHeroSubtitle,
+            {
+                opacity: 0,
+                duration: 3,
+                y: 0,
+            },
+            {
+                opacity: 1,
+                duration: 2,
+                y: 20,
+                ease: "power4.out",
+            }
+        );
+    }, []);
+
+    return (
+        <div className="px-6">
+            <div className="mx-auto my-64 relative max-w-screen-md">
+                <hr className={`w-20 absolute right-1 -top-10 ${css.line}`} />
+                <ImageHero />
+                <p className="mt-5 text-base lg:text-3xl opacity-0" ref={textHeroSubtitleRef}>
+                    {t("heroSubtitle")}
+                </p>
+                <hr className={`w-40 absolute right-0 ${css.line}`} />
+            </div>
+            <Detail />
+        </div>
     );
-  }, []);
-
-  return (
-    <div className="px-6">
-      <div className="mx-auto my-64 relative max-w-screen-md">
-        <hr className={`w-20 absolute right-1 -top-10 ${css.line}`} />
-        <ImageHero />
-        <p className="mt-5 text-base lg:text-3xl opacity-0" ref={textHeroSubtitleRef}>
-          {t("heroSubtitle")}
-        </p>
-        <hr className={`w-40 absolute right-0 ${css.line}`} />
-      </div>
-      <Detail />
-    </div>
-  );
 };
 
 export default Hero;
