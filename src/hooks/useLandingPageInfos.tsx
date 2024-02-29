8caa117070012b4545a24ce5ab5106893db3438c
Edson Costa <61160635+ecsistem@users.noreply.github.com>
2024-02-29 16:13:51 -0300

    fear: eventos passados - Resolve #8 (#64)
    * feat: gallery add
    
    * docs: i added the project link in the readme
    
    * [ImgBot] Optimize images
    
    *Total -- 2,829.49kb -> 2,500.35kb (11.63%)
    
    /public/images/eventos.png -- 629.60kb -> 525.25kb (16.57%)
    /public/images/palestras.png -- 588.45kb -> 517.38kb (12.08%)
    /public/images/networking.png -- 811.50kb -> 733.22kb (9.65%)
    /public/images/sorteios.png -- 799.95kb -> 724.50kb (9.43%)
    
    Signed-off-by: ImgBotApp <ImgBotHelp@gmail.com>
    
    * feat(events): add eventos passados
    
    ---------
    
    Signed-off-by: ImgBotApp <ImgBotHelp@gmail.com>
    Co-authored-by: ImgBotApp <ImgBotHelp@gmail.com>

--- src/hooks/useLandingPageInfos.tsx
+++ src/hooks/useLandingPageInfos.tsx
@@ -1,46 +1,8 @@
-import { EventList } from "@/components/Events";
-import { useEffect, useState } from "react";
+import { EventContent } from "@/components/Events/EventContent";
 import { useTranslation } from "next-i18next";
 
 export const useLandingPageInfos = () => {
   const { t } = useTranslation();
-  const [events, setEvents] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://www.sympla.com.br/api/v1/search", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            service: "/v4/search",
-            params: {
-              only: "name,images,location,start_date_formats,end_date_formats,url",
-              organizer_id: 5478152,
-              sort: "date",
-              order_by: "desc",
-              limit: "3",
-              page: 1,
-            },
-            ignoreLocation: true,
-          }),
-        });
-
-        if (!response.ok) {
-          throw new Error("Erro ao buscar eventos");
-        }
-
-        const data = await response.json();
-        setEvents(data.data);
-      } catch (error) {
-        console.error("Erro ao buscar eventos:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   const infos = [
     {
       image: {
@@ -72,8 +34,8 @@ export const useLandingPageInfos = () => {
         url: "/images/eventos.png",
         alt: "Eventos Sympla",
       },
-      title: t("nextEvents"),
-      info: <>{events.length === 0 ? <p>{t("noEvents")}</p> : <EventList events={events} />}</>,
+      title: t("events"),
+      info: <>{<EventContent /> }</>,
     },
   ];
   return infos;
