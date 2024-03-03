ac678a7567065ef5c174664be43d319fbfb7ec1e
Kedgard Cordero <GeckoB4I@gmail.com>
2024-03-03 12:25:05 -0600

    fix: Solved Hydration: Issue #73 (#74)
    * solve hydration issue
    
    * adjust capitalization
    
    * solved available and closed working issue

--- src/components/Events/EventContent.tsx
+++ src/components/Events/EventContent.tsx
@@ -1,29 +1,31 @@
+"use client";
+
 // EventContent.tsx
 import { useEventFetcher } from "@/hooks/useEventFetcher";
 import { useTranslation } from "react-i18next";
 import { EventList } from ".";
+import { useEffect, useState } from "react";
 
 export function EventContent() {
     const { t } = useTranslation();
     const { events, eventType, toggleEventType } = useEventFetcher();
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
     return (
         <div>
             <div className="flex justify-center mb-4">
-                <button
-                    onClick={() => toggleEventType()}
-                    className={`px-4 py-2 mx-2 text-sm font-medium text-black ${eventType === 'future' ? 'bg-green-300' : 'bg-green-200 hover:bg-green-300'}`}
-                >
-                    {t("available")}
+                <button onClick={() => toggleEventType()} className={`px-4 py-2 mx-2 text-sm font-medium text-black ${eventType === "future" ? "bg-green-300" : "bg-green-200 hover:bg-green-300"}`}>
+                    {isClient && t("available")}
                 </button>
-                <button
-                    onClick={() => toggleEventType()}
-                    className={`px-4 py-2 mx-2 text-sm font-medium text-black ${eventType === 'past' ? 'bg-green-300' : 'bg-green-200 hover:bg-green-300'}`}
-                >
-                     {t("closed")}
+                <button onClick={() => toggleEventType()} className={`px-4 py-2 mx-2 text-sm font-medium text-black ${eventType === "past" ? "bg-green-300" : "bg-green-200 hover:bg-green-300"}`}>
+                    {isClient && t("closed")}
                 </button>
             </div>
-            {events.length === 0 ? <p>{t("noEvents")}</p> : <EventList events={events} />}
+            {isClient && (events.length === 0 ? <p>{t("noEvents")}</p> : <EventList events={events} />)}
         </div>
     );
 }
