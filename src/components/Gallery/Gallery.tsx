f19600f768ac6a15bdc1cbdad50fe7a75c8e83de
daywison <daywisonsilvasou@gmail.com>
2024-04-05 07:52:16 -0300

    fix: remove unused gsap and add some animations

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,145 +1,143 @@
-import { GalleryProps } from "@/types/gallery";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useTranslation } from "next-i18next";
-import Image from "next/image";
-import { FC, useEffect, useRef, useState } from "react";
-import LineSection from "../../../public/icons/line-section.svg";
-import { Title } from "../Section";
+import { FC, useEffect, useRef, useState } from 'react'
+import { GalleryProps } from '@/types/gallery'
+import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
+// import LineSection from '../../../public/icons/line-section.svg'
+import { Title } from '../Section'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Gallery: FC<GalleryProps> = ({ images }) => {
-    const { t } = useTranslation();
-    const galleryRef = useRef<HTMLDivElement>(null);
-    const [windowWidth, setWindowWidth] = useState<number>(0);
+  const { t } = useTranslation()
+  const [windowWidth, setWindowWidth] = useState<number>(0)
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
 
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+    handleResize() // Define a largura inicial da janela
+    window.addEventListener('resize', handleResize)
 
-        handleResize(); // Define a largura inicial da janela
-        window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+  const calculateGroups = (windowWidth: number) => (windowWidth < 1024 ? 3 : 6)
 
-    useEffect(() => {
-        if (galleryRef.current) {
-            gsap.from(galleryRef.current.children, {
-                opacity: 0.7,
-                x: (index) => (index % 2 === 0 ? -350 : 350),
-                stagger: 0.1,
-                duration: 1,
-                ease: "power3.out",
-                scrollTrigger: {
-                    trigger: galleryRef.current,
-                    start: "top 30%", // Começa a animação quando o elemento está 80% visível na janela
-                    end: "bottom 20%", // Termina a animação quando o elemento está 20% visível na janela
-                    scrub: true, // Suaviza o movimento de acordo com o scroll
-                    markers: false, // Remove os marcadores do ScrollTrigger
-                },
-            }),
-                gsap.to(galleryRef.current.children, {
-                    opacity: 1,
-                    x: 0,
-                    stagger: 0.8,
-                    duration: 1,
-                    ease: "power3.out",
-                    scrollTrigger: {
-                        trigger: galleryRef.current,
-                        start: "top 30%", // Começa a animação quando o elemento está 80% visível na janela
-                        end: "bottom 20%", // Termina a animação quando o elemento está 20% visível na janela
-                        scrub: true, // Suaviza o movimento de acordo com o scroll
-                        markers: false, // Remove os marcadores do ScrollTrigger
-                    },
-                });
-        }
-    }, [windowWidth, images]);
+  const imageGroups = images.reduce((acc: string[][], image, index) => {
+    const groupIndex = Math.floor(index / calculateGroups(windowWidth))
+    acc[groupIndex] = [...(acc[groupIndex] || []), image]
+    return acc
+  }, [])
 
-    const calculateGroups = (windowWidth: number) => (windowWidth < 1024 ? 3 : 6);
+  //* Testing out modal
 
-    const imageGroups = images.reduce((acc: string[][], image, index) => {
-        const groupIndex = Math.floor(index / calculateGroups(windowWidth));
-        acc[groupIndex] = [...(acc[groupIndex] || []), image];
-        return acc;
-    }, []);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentImage, setCurrentImage] = useState(0)
+  const [currentGroup, setCurrentGroup] = useState(0)
 
-    //* Testing out modal
+  const openModal = (groupIndex: number, imageIndex: number) => {
+    setCurrentGroup(groupIndex)
+    setCurrentImage(imageIndex)
+    setIsModalOpen(true)
+  }
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentImage, setCurrentImage] = useState(0);
-    const [currentGroup, setCurrentGroup] = useState(0);
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
 
-    const openModal = (groupIndex: number, imageIndex: number) => {
-        setCurrentGroup(groupIndex);
-        setCurrentImage(imageIndex);
-        setIsModalOpen(true);
-    };
+  const goToNextImage = () => {
+    // Check if next image is in the current group
+    if (currentImage < imageGroups[currentGroup].length - 1) {
+      setCurrentImage(currentImage + 1)
+    } else if (currentGroup < imageGroups.length - 1) {
+      // Move to the next group
+      setCurrentGroup(currentGroup + 1)
+      setCurrentImage(0)
+    }
+  }
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+  const goToPreviousImage = () => {
+    if (currentImage > 0) {
+      setCurrentImage(currentImage - 1)
+    } else if (currentGroup > 0) {
+      // Move to the previous group
+      setCurrentGroup(currentGroup - 1)
+      setCurrentImage(imageGroups[currentGroup - 1].length - 1)
+    }
+  }
 
-    const goToNextImage = () => {
-        // Check if next image is in the current group
-        if (currentImage < imageGroups[currentGroup].length - 1) {
-            setCurrentImage(currentImage + 1);
-        } else if (currentGroup < imageGroups.length - 1) {
-            // Move to the next group
-            setCurrentGroup(currentGroup + 1);
-            setCurrentImage(0);
-        }
-    };
-
-    const goToPreviousImage = () => {
-        if (currentImage > 0) {
-            setCurrentImage(currentImage - 1);
-        } else if (currentGroup > 0) {
-            // Move to the previous group
-            setCurrentGroup(currentGroup - 1);
-            setCurrentImage(imageGroups[currentGroup - 1].length - 1);
-        }
-    };
-
-    return (
-        <div className="my-12">
-            <div className="px-5 lg:px-48 my-8">
-                <Title>{t("gallery")}</Title>
-                <div className="pt-5">
-                    <LineSection className="w-[157px] h-[1px] text-white" />
-                </div>
-            </div>
-            <div className="overflow-hidden">
-                {imageGroups.map((group, groupIndex) => (
-                    <div key={groupIndex} className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-4 md:gap-4 my-4">
-                        {group.map((image, index) => (
-                            <div key={index} className="w-full overflow-hidden cursor-pointer" onClick={() => openModal(groupIndex, index)}>
-                                <Image src={image} alt={`Gallery Image ${index + 1}`} width={800} height={600} layout="responsive" objectFit="cover" />
-                            </div>
-                        ))}
-                    </div>
-                ))}
-            </div>
-            {isModalOpen && (
-                <div className="modal" onClick={closeModal}>
-                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                        <span className="close" onClick={closeModal}>
-                            &times;
-                        </span>
-                        <Image src={imageGroups[currentGroup][currentImage]} alt="Modal Image" width={800} height={600} layout="responsive" objectFit="cover" />
-                        <div className="mt-5 flex justify-between">
-                            <button onClick={goToPreviousImage}>Previous</button>
-                            <button onClick={goToNextImage}>Next</button>
-                        </div>
-                    </div>
-                </div>
-            )}
+  return (
+    <div className='my-12'>
+      <div className='px-5 lg:px-48 my-8'>
+        <Title>{t('gallery')}</Title>
+        <div className='pt-5'>
+          {/* <LineSection className='w-[157px] h-[1px] text-white' /> */}
         </div>
-    );
-};
+      </div>
+      <div className='overflow-hidden'>
+        {imageGroups.map((group, groupIndex) => (
+          <div
+            key={groupIndex}
+            className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-4 md:gap-4 my-4'
+          >
+            {group.map((image, index) => (
+              <div
+                key={index}
+                className='w-full overflow-hidden cursor-pointer'
+                onClick={() => openModal(groupIndex, index)}
+              >
+                <Image
+                  src={image}
+                  alt={`Gallery Image ${index + 1}`}
+                  width={800}
+                  height={600}
+                  layout='responsive'
+                  objectFit='cover'
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <AnimatePresence>
+        {isModalOpen && (
+          <motion.div
+            className='fixed top-0 left-0 flex items-center justify-center w-full h-screen bg-[#0008] backdrop-blur-md'
+            onClick={closeModal}
+            exit={{ opacity: 0, scale: 0.8, backgroundColor: 'transparent' }}
+          >
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              key={imageGroups[currentGroup][currentImage]}
+              className='max-w-sm w-full h-[calc(100%-200px)] relative'
+              onClick={(e) => e.stopPropagation()}
+            >
+              <span
+                className='absolute top-2 right-2 text-4xl cursor-pointer w-8 h-8 flex items-center justify-center border-2 border-brand rounded-full bg-[#0007]'
+                onClick={closeModal}
+              >
+                &times;
+              </span>
+              <Image
+                src={imageGroups[currentGroup][currentImage]}
+                alt='Modal Image'
+                width={630}
+                height={835}
+                className='w-full object-cover h-full aspect-[0.75/1] grow-0'
+              />
+              <div className='mt-5 flex justify-between'>
+                <button onClick={goToPreviousImage}>Previous</button>
+                <button onClick={goToNextImage}>Next</button>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
 
-export default Gallery;
+export default Gallery
