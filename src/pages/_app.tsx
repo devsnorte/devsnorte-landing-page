a7f404ad3970a04a99a59e7a876fa13021d84b06
Patrick <eng.patrickmonteiro@gmail.com>
2024-05-16 19:16:20 -0300

    fix(css): fix import css build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { appWithTranslation } from "next-i18next";
 import { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "300", "400", "700", "600"], variable: "--font-poppins" });
-import "./globals.css";
+import "./globals.scss";
 import { ThemeProvider } from "@/components/ContextAPI/ThemeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
